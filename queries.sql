e1f7bd40cd8070f45805c163514075afc226754a
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-08-09 22:35:34 +0300

    Add all requirements to repo

--- queries.sql
+++ queries.sql
@@ -1,3 +1,14 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * from animals WHERE name = 'Luna';
+
+SELECT * FROM public.animals
+    WHERE name like '%mon'
+
+SELECT * FROM public.animals
+    WHERE date_of_birth between '2016-01-01' and '2019-12-30'
+
+SELECT * FROM public.animals
+    WHERE neutered = true and escape_attempts < 3
+
+SELECT date_of_birth FROM public.animals
+  WHERE name = 'Agumon' OR name = 'Pikachu'
