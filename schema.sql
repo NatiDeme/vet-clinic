cd922203056f3dacaa74ffd3e168c23580abfe34
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-08-12 10:34:46 +0300

    Add primary key and foriegn key establishing relations

--- schema.sql
+++ schema.sql
@@ -1,17 +1,17 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE woners (
-    id INT GENERATED ALWAYS AS IDENTITY,
+CREATE TABLE owners (
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     full_name VARCHAR(50),
     age integer
 );
 
 CREATE TABLE species (
-    id INT GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(50),
+    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50)   
 );
 
-DROP TABLE IF EXISTS animals;
+
 CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(50),
@@ -19,7 +19,12 @@ CREATE TABLE animals (
     escape_attempts integer,
     neutered boolean,
     weight_kg numeric,
+    owner_id INT,
+    species_id INT,
     CONSTRAINT fk_species
         FOREIGN KEY(species_id)
-            REFERENCES species(id)
+            REFERENCES species(id),
+    CONSTRAINT fk_owner
+        FOREIGN KEY(owner_id)
+            REFERENCES owners(id)
 );
