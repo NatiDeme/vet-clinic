0f2266432d89736c0e8ff42dc1006e1b56b7665c
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-08-12 10:19:51 +0300

    Add schema for the three tables

--- schema.sql
+++ schema.sql
@@ -1,11 +1,25 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    id integer,
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(50),
+    age integer
+);
+
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(50),
+);
+
+DROP TABLE IF EXISTS animals;
+CREATE TABLE animals (
+    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(50),
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
     weight_kg numeric,
-    species VARCHAR(50)
-)
+    CONSTRAINT fk_species
+        FOREIGN KEY(species_id)
+            REFERENCES species(id)
+);
