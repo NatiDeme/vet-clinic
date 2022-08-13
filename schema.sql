5fae06864d109cc23f97e4482970816a88f53c4b
Natnael Demelash <natidemelashnatnael@hotmail.com>
2022-08-13 07:38:19 +0300

    Add the table schema to codebase

--- schema.sql
+++ schema.sql
@@ -1,30 +1,27 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE owners (
+CREATE TABLE vets (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    full_name VARCHAR(50),
-    age integer
-);
-
-CREATE TABLE species (
-    id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    name VARCHAR(50)   
-);
-
-
-CREATE TABLE animals (
-    id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(50),
-    date_of_birth date,
-    escape_attempts integer,
-    neutered boolean,
-    weight_kg numeric,
-    owner_id INT,
-    species_id INT,
-    CONSTRAINT fk_species
-        FOREIGN KEY(species_id)
-            REFERENCES species(id),
-    CONSTRAINT fk_owner
-        FOREIGN KEY(owner_id)
-            REFERENCES owners(id)
+    age INT,
+    date_of_graduation date
+);
+
+
+CREATE TABLE specializations (
+    vets_id        INT,
+    species_id     INT, 
+    PRIMARY KEY(vets_id, species_id),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id),
+    CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id)
+);
+
+
+CREATE TABLE visits (
+    animals_id    INT,
+    vets_id       INT,
+    date_of_visit DATE, 
+    PRIMARY KEY(animals_id, vets_id, date_of_visit),
+    CONSTRAINT fk_animals FOREIGN KEY(animals_id) REFERENCES animals(id),
+    CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
 );
